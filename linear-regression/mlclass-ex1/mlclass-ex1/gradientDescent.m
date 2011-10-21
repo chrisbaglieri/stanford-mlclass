155705bad43aa2c1d570b8ca33a89310c619dcc6
Chris Baglieri <chris.baglieri@gmail.com>
2011-10-21 14:38:09 -0400

    finished up gradient descent implementation

--- linear-regression/mlclass-ex1/mlclass-ex1/gradientDescent.m
+++ linear-regression/mlclass-ex1/mlclass-ex1/gradientDescent.m
@@ -17,11 +17,10 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
 
-
-
-
-
-
+	htheta = X * theta;
+	theta_0 = theta(1) - alpha / m * sum((htheta - y) .* X(:,1));
+	theta_1 = theta(2) - alpha / m * sum((htheta - y) .* X(:,2));
+	theta = [theta_0; theta_1];
 
     % ============================================================
 
